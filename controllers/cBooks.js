6c1e18c38e414fae50438899b042b9c6418e8222
eben4ya <benaya.imanuela@gmail.com>
2024-06-05 11:57:36 +0700

    <feat: added Book controller>

--- controllers/cBooks.js
+++ controllers/cBooks.js
@@ -1,5 +1,18 @@
-
-
-// endpoint to get all books
+const db = require("../db/index");
 
 // endpoint to add new book
+exports.addBook = async (req, res) => {
+  const { title, publicationYear, pages, publisherID } = req.body;
+  try {
+    const { rows } = await db.query(
+      "INSERT INTO Book (Title, PublicationYear, Pages, PublisherID) VALUES ($1, $2, $3, $4) RETURNING *",
+      [title, publicationYear, pages, publisherID]
+    );
+    res.status(201).json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// endpoint to get all books
