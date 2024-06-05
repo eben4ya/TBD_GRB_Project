99021c02e8e685a2609134a6ac5c8c2baffc0557
eben4ya <benaya.imanuela@gmail.com>
2024-06-05 13:09:04 +0700

    <feat: added book publisher transaction>

--- controllers/cBooks.js
+++ controllers/cBooks.js
@@ -2,14 +2,43 @@ const db = require("../db/index");
 
 // endpoint to add new book
 exports.add_Book = async (req, res) => {
-  const { title, publicationYear, pages, publisherID } = req.body;
+  const {
+    publisherName,
+    city,
+    country,
+    telephone,
+    yearFounded,
+    title,
+    publicationYear,
+    pages,
+  } = req.body;
   try {
-    const { rows } = await db.query(
+    // begin transaction
+    await db.query("BEGIN");
+
+    // insert into Publisher
+    const publisherResult = await db.query(
+      "INSERT INTO Publisher (Name, City, Country, Telephone, YearFounded) VALUES ($1, $2, $3, $4, $5) RETURNING PublisherID ",
+      [publisherName, city, country, telephone, yearFounded]
+    );
+
+    const publisherID = publisherResult.rows[0].publisherid;
+
+    // insert into Book
+    const bookResult = await db.query(
       "INSERT INTO Book (Title, PublicationYear, Pages, PublisherID) VALUES ($1, $2, $3, $4) RETURNING *",
       [title, publicationYear, pages, publisherID]
     );
-    res.status(201).json(rows[0]);
+
+    // commit transaction
+    await db.query("COMMIT");
+
+    res
+      .status(201)
+      .json({ publisher: publisherResult.rows[0], book: bookResult.rows[0] });
   } catch (err) {
+    // rollback transaction
+    await db.query("ROLLBACK");
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
   }
