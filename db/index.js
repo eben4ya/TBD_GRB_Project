a99d7bae7a909940f0fb2e855eec3d3b52e52af1
eben4ya <benaya.imanuela@gmail.com>
2024-06-05 10:40:03 +0700

    <feat: added connection to postgresql>

--- db/index.js
+++ db/index.js
@@ -1,1 +1,10 @@
 // DB Connection
+
+const { Pool } = require("pg");
+const config = require("../config");
+
+const pool = new Pool(config.db);
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
